928736c3dfa58c9eca8f15ae6407f2085ca19988
SaarthakGitHub <saarthak173@gmail.com>
2024-09-01 16:52:06 +0530

    Code till video3

---
+++ src/components/BasicTable.jsx
@@ -0,0 +1,47 @@
+import React, { useMemo } from 'react';
+import { useTable } from "react-table";
+import Sample_Data from './MOCK_DATA.json';
+import COLUMNS from "./columns";
+import './table.css';
+
+const BasicTable = () => {
+    const columns = useMemo(() => COLUMNS, []);
+    const data = useMemo(() => Sample_Data, []);
+    const {
+        getTableProps,
+        getTableBodyProps,
+        headerGroups,
+        rows,
+        prepareRow,
+    } = useTable({
+        columns,
+        data
+    })
+  return (
+    <table {...getTableProps()}>
+        <thead>
+        {
+            headerGroups.map((headerGroup) => (
+                <tr {...headerGroup.getHeaderGroupProps()} >
+                    { headerGroup.headers.map(column => (
+                        <th {...column.getHeaderProps()}>{column.render('Header')}</th>
+            ))}
+                </tr>
+            ))
+        }
+        </thead>
+        <tbody {...getTableBodyProps()}>
+        {rows.map(row => { prepareRow(row)
+        return (
+            <tr {...row.getRowProps()}>
+                {row.cells.map(cell => {
+                    return <td {...cell.getCellProps()}>{cell.render('Cell')}</td>
+                })}
+            </tr>
+        )})}
+        </tbody>
+    </table>
+  )
+}
+
+export default BasicTable
